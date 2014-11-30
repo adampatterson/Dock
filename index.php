150c9397d2c7ab39affd2d9dd3d75b5ddcc5c5d3
Adam Patterson <hello@adampatterson.ca>
2014-11-30 12:00:43 -0700

    Structure Added

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
 					<header>
 						<h1>Dock</h1>
 						<h2>A non restrictive framework</h2>
-						<p class="lead">Dock is a simple Bootstrap based framework made to be as non restrictive as possible. Taking a focusing layout rather than style, after all that is your job!</p>
+						<p class="lead">Dock is a simple Bootstrap based framework made to be as non restrictive as possible. Taking a focus on layout rather than style, after all that is your job!</p>
 						<p class="lead">You won't find any of the Icing that bootstrap has but rather a more concise grid system with centring, a more functional file structure allowing you to extend rather than over ride styles.</p>
 						<p class="lead">Made to be extensible, it's possible to add your own button code or include what other UI frameworks have already made.</p>
 					</header>
